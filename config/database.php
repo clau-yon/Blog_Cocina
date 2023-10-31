f9c50e7939fc78830e316cf5914a1396ba8b7986
clau-yon <clau.yon.15@mi.unc.edu.ar>
2023-10-30 23:13:01 -0300

    Modificado y funcionando

--- config/database.php
+++ config/database.php
@@ -1,16 +1,21 @@
 <?php
    
-            $host = "localhost";
-            $dbname = "tarea27";
-            $username = "root";
-            $password = "";
+    $host = "localhost";
+    $dbname = "tarea27";
+    $username = "root";
+    $password = "";
 
-            try {
+          /* try {
                 $db = new PDO("mysql:host=$host;dbname=$dbname;charset=utf8", $username, $password);
                 $db->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
             } catch (PDOException $e) {
                 die("Error de conexión a la base de datos: " . $e->getMessage());
-            }
+            }*/
+            $conn = mysqli_connect($host, $username, $password, $dbname);
 
+            if (!$conn) {
+                die("Error de conexión a la base de datos: " . mysqli_connect_error());
+            }
+            
 
 ?>
