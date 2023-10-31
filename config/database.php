44a190b25df59c234ee02a8c0e022a649657efcb
PabloContreras <Contreras.pabloarg@hotmail.com>
2023-10-31 17:57:03 -0300

    algo

--- config/database.php
+++ config/database.php
@@ -1,6 +1,6 @@
 <?php
    
-            $host = "localhost";
+            $host = "localhost:3307";
             $dbname = "tarea27";
             $username = "root";
             $password = "";
--- index.php
+++ index.php
@@ -7,15 +7,7 @@
           <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.2.3/dist/js/bootstrap.bundle.min.js" integrity="sha384-kenU1KFdBIe4zVF0s0G1M5b4hcpxyD9F7jL+jjXkk+Q2h455rYXK/7HAuoJl+0I4" crossorigin="anonymous"></script>
           <script src="https://kit.fontawesome.com/6e316b87b7.js" crossorigin="anonymous"></script>
     </head>
-  <!--  <body>
-    
-    <div class="navbar">
-        <h1>Name Blog</h1>
-        <a href="index.html">Home</a>
-        <a href="about.html">About</a>
-        <a href="contact.html">Contact</a>
-        <a href="contact.html">Login</a>
-    </div>-->
+
 <?php include('./components/header.php'); ?>    
     <!-- INICIO DEL CARROSEL -->
     <div class="cards">
